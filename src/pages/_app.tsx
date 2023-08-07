a89e0c71ac659e6863e527be7a6f54bbb098fcf3
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2023-08-07 16:47:01 -0500

    added middleware, and a signIn component to my next.js app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default MyApp;
